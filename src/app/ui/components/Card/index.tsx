f6857d4619539b7c3bfd07ff4bfc41720cc50b3e
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-04-17 15:47:10 +0200

    add Component Card

---
+++ src/app/ui/components/Card/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+type CardProps = {
+  img: string;
+  name: string;
+  price: string;
+};
+
+function Card({ img, name, price }: CardProps) {
+  return (
+    <div>
+      <Image src={img} alt={name} width={1000} height={700} />
+      <div className="p-[10px]">
+        <div className="text-[13px] color-[#121314]">{name}</div>
+        <div className="text-[13px] color-[#121314]">{price} €</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
